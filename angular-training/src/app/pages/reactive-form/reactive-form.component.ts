d67eca7f6de48e470873bb9744c43bd43fa93c49
Sama Gyaneshwar <sama.gyaneshwar@sourcefuse.com>
2022-06-09 14:24:58 +0530

    feat: implement reactive form

--- angular-training/src/app/pages/reactive-form/reactive-form.component.ts
+++ angular-training/src/app/pages/reactive-form/reactive-form.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
+  user: any = {
+    name: '',
+    email: '',
+    gender: '',
+    note: '',
+    agreeTc: false,
+  };
+  userForm: FormGroup;
+  genders: string[] = ['Male', 'Female', 'Other'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.userForm = new FormGroup({
+      name: new FormControl(this.user.name, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      email: new FormControl(this.user.email, [
+        Validators.required,
+        Validators.email,
+      ]),
+      gender: new FormControl(this.user.gender, [Validators.required]),
+      note: new FormControl(this.user.note),
+      agreeTc: new FormControl(this.user.agreeTc, [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('[FormSubmitted]', this.userForm.value);
+  }
+
+  control(name: string) {
+    return this.userForm.get(name);
+  }
 }
